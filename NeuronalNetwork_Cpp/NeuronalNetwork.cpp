0ac61ae295a204979ce8cab20ba490cfda7e81ef
ingmacmech <marcus.schluep@kns.ch>
2017-03-23 21:26:06 +0100

    added NeuronalNetwork class

---
+++ NeuronalNetwork_Cpp/NeuronalNetwork.cpp
@@ -0,0 +1,101 @@
+/*****************************************************************************/
+/*  Class      : NeuronalNetwork                                Version 1.0  */
+/*****************************************************************************/
+/*                                                                           */
+/*  Function   : This class describes the Neuronal Network			         */
+/*                                                                           */
+/*                                                                           */
+/*  Methodes   :							                                 */
+/*										                                     */
+/*																			 */
+/*																			 */
+/*                                                                           */
+/*  Author     : ingmacmech                                                  */
+/*                                                                           */
+/*  History    : 23.03.20017    Created                                      */
+/*                                                                           */
+/*  File       : NeuronalNetwork.cpp    		                             */
+/*                                                                           */
+/*****************************************************************************/
+/* Artificial Neuronal Network                                               */
+/*****************************************************************************/
+
+/* imports */
+#include "NeuronalNetwork.h"
+#include <iostream>
+#include <armadillo>
+
+using namespace arma;
+
+/* Class constant declaration  */
+
+/* Class Type declaration      */
+
+/* Class data declaration      */
+
+
+/* Class procedure declaration */
+
+
+/*****************************************************************************/
+/*  Method      : NeuronalNetwork                                                    */
+/*****************************************************************************/
+/*                                                                           */
+/*  Function    : Constructor for the NeuronalNetwork class                  */
+/*                                                                           */
+/*  Type        : Constructor                                                */
+/*                                                                           */
+/*  Input Para  : None								                         */
+/*                                                                           */
+/*  Output Para : None                                                       */
+/*                                                                           */
+/*  Author      : ingmacmech                                                 */
+/*                                                                           */
+/*  History     : 23.03.2017     Created                                     */
+/*                                                                           */
+/*****************************************************************************/
+
+NeuronalNetwork::NeuronalNetwork()
+{
+
+}
+
+/*****************************************************************************/
+/*  End  Method : InputNN                                                    */
+/*****************************************************************************/
+
+
+
+/*****************************************************************************/
+/*  Method      : Destructor                                                 */
+/*****************************************************************************/
+/*                                                                           */
+/*  Function    :   Clean up resources				                         */
+/*                                                                           */
+/*  Type        : Public                                                     */
+/*                                                                           */
+/*  Input Para  : None								                         */
+/*                                                                           */
+/*  Output Para : None                                                       */
+/*                                                                           */
+/*  Author      : ingmacmech                                                 */
+/*                                                                           */
+/*  History     : 28.02.2017     Created                                     */
+/*                                                                           */
+/*****************************************************************************/
+NeuronalNetwork::~NeuronalNetwork()
+{
+	std::cout << "\nNeuronalNetwork destroyed\n" << std::endl;
+
+}
+
+/*****************************************************************************/
+/*  End  Method : Destructor                                                 */
+/*****************************************************************************/
+
+
+
+
+/*****************************************************************************/
+/*  End Class   : NeuronalNetwork                                            */
+/*****************************************************************************/
---
+++ NeuronalNetwork_Cpp/NeuronalNetwork.h
@@ -0,0 +1,62 @@
+#ifndef NEURONALNETWORK_CLASS_HPP
+#define NEURONALNETWORK_CLASS_HPP
+/*****************************************************************************/
+/*  Header     : NeuronalNetwork                                Version 1.0  */
+/*****************************************************************************/
+/*                                                                           */
+/*  Function   : This class describes the Neuronal Network				     */
+/*                                                                           */
+/*                                                                           */
+/*  Methodes   :					                                         */
+/*																			 */
+/*									                                         */
+/*									                                         */
+/*									                                         */
+/*                                                                           */
+/*  Author     : ingmacmech                                                  */
+/*                                                                           */
+/*  History    : 23.03.2017  IO Created                                      */
+/*                                                                           */
+/*  File       : NeuronalNetwork.hpp                                                 */
+/*                                                                           */
+/*****************************************************************************/
+/* Artificial Neuronal Network                                               */
+/*****************************************************************************/
+
+/* imports */
+#include<armadillo>
+using namespace arma;
+/* Class constant declaration  */
+
+/* Class Type declaration      */
+
+/* Class data declaration      */
+
+/* Class definition            */
+
+
+class NeuronalNetwork {
+
+	// Data
+	
+	// Methods
+
+
+public:
+	// Constructor
+	NeuronalNetwork();
+
+	// Copy Constructor
+
+
+	// Destructor
+	~NeuronalNetwork();
+
+	// Methode
+	
+};
+
+/*****************************************************************************/
+/*  End Header  : LineClass                                                  */
+/*****************************************************************************/
+#endif
